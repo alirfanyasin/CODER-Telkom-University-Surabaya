cee78e4a8e17f1ecfbd1b2144c9be86542c728ca
alirfanyasin <risayunita21@gmail.com>
2024-07-05 14:13:43 +0700

    installation preline

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -1,6 +1,6 @@
-<div class="h-screen flex items-center justify-center">
+<div class="flex items-center justify-center h-screen">
   <div>
-    <h1 class="text-4xl font-bold text-slate-600 text-center mb-10">Welcome to web CODER V.2</h1>
+    <h1 class="mb-10 text-4xl font-bold text-center text-slate-600">Welcome to web CODER V.2</h1>
     <livewire:components.button title="Login" route="login">
   </div>
 </div>
