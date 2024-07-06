7b088d6c77bc4c3e103a80321816c0712cbf8ec6
Dandymay <dandyainul19@gmail.com>
2024-07-06 23:45:49 +0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ Route::get('/', Home::class)->name('home');
 
 // Auth Route
 Route::get('/login', Login::class)->name('login');
+Route::get('/register', Login::class)->name('login');
+
 
 // Authenticated - User
 Route::prefix('app')->group(function () {
