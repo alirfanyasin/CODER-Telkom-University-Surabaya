76dc9c0a6355cf81ae097b486ebd6d4b51b0607c
alirfanyasin <risayunita21@gmail.com>
2024-07-06 21:41:55 +0700

    slicing: division, gallery and article section home page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,11 @@
 
   <title>{{ $title . ' | CODER' ?? 'CODER' }}</title>
 
+  {{-- JQuery --}}
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  {{-- Slick --}}
+  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+
   {{-- Vite --}}
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @livewireStyles
@@ -21,7 +26,11 @@
 
   {{-- Iconify --}}
   <script src="https://cdn.jsdelivr.net/npm/iconify-icon@2.1.0/dist/iconify-icon.min.js"></script>
+  {{-- Slick --}}
+  <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
   @livewireScripts
+
+  @stack('js-custom')
 
 </body>
 
--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -37,7 +37,7 @@
   <section class="my-32">
     <div class="container">
       <div class="w-10/12 mx-auto">
-        <div class="grid items-center justify-between grid-cols-4">
+        <div class="flex items-center justify-between">
           <img src="assets/images/logo/playbox.png" alt="Logo PlayBox" class="w-56">
           <img src="assets/images/logo/waow.png" alt="Logo Waow" class="w-56">
           <img src="assets/images/logo/wezaa.png" alt="Logo Weza" class="w-56">
@@ -56,7 +56,7 @@
         <div class="flex items-center ">
           <div class="basis-4/12">
             <div class="flex justify-center">
-              <h1 class="text-5xl font-bold leading-tight text-white">Tentang <br> Kami</h1>
+              <h2 class="text-5xl font-bold leading-tight text-white">Tentang <br> Kami</h2>
             </div>
           </div>
           <div class="basis-8/12">
@@ -88,4 +88,209 @@
   </section>
   {{-- About Us Section End --}}
 
+
+
+  {{-- Division Section Start --}}
+  <section class="mb-56 mt-80">
+    <div class="container">
+      <div class="w-10/12 mx-auto">
+        <header class="flex items-center justify-center mb-20 text-center">
+          <div class="border border-gray-700 w-36"></div>
+          <h2 class="mx-5 text-5xl font-bold text-white">Divisi</h2>
+          <div class="border border-gray-700 w-36"></div>
+        </header>
+
+        <div class="grid grid-cols-2 gap-6">
+          <div class="p-8 bg-glass rounded-xl backdrop-blur-sm">
+            <div class="flex items-center">
+              <div class="text-white basis-2/12">
+                <iconify-icon icon="quill:desktop" class="text-6xl"></iconify-icon>
+              </div>
+              <div class="basis-10/12">
+                <h4 class="text-2xl font-bold text-white">Web Development</h4>
+                <p class="text-gray-400">Belajar dan mengembangan aplikasi berbasis Website yang modern</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-8 bg-glass rounded-xl backdrop-blur-sm">
+            <div class="flex items-center">
+              <div class="text-white basis-2/12">
+                <iconify-icon icon="mingcute:pen-line" class="text-6xl"></iconify-icon>
+              </div>
+              <div class="basis-10/12">
+                <h4 class="text-2xl font-bold text-white">UI/UX Design</h4>
+                <p class="text-gray-400">Melakukan proses Sketching, Wireframing, Mockup, hingga Prototype</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-8 bg-glass rounded-xl backdrop-blur-sm">
+            <div class="flex items-center">
+              <div class="text-white basis-2/12">
+                <iconify-icon icon="mage:robot" class="text-6xl"></iconify-icon>
+              </div>
+              <div class="basis-10/12">
+                <h4 class="text-2xl font-bold text-white">AI Software</h4>
+                <p class="text-gray-400">Mengembangkan perangkat lunak berbasis kecerdasan artifisial</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-8 bg-glass rounded-xl backdrop-blur-sm">
+            <div class="flex items-center">
+              <div class="text-white basis-2/12">
+                <iconify-icon icon="nimbus:mobile" class="text-6xl"></iconify-icon>
+              </div>
+              <div class="basis-10/12">
+                <h4 class="text-2xl font-bold text-white">Mobile Development</h4>
+                <p class="text-gray-400">Melakukan pengembangan aplikasi berbasis Mobile Application</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-8 bg-glass rounded-xl backdrop-blur-sm">
+            <div class="flex items-center">
+              <div class="text-white basis-2/12">
+                <iconify-icon icon="ph:code-bold" class="text-6xl"></iconify-icon>
+              </div>
+              <div class="basis-10/12">
+                <h4 class="text-2xl font-bold text-white">Competitive Programming</h4>
+                <p class="text-gray-400">Melakukan pengembangan algoritma pemrograman untuk berkompetisi</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-8 bg-glass rounded-xl backdrop-blur-sm">
+            <div class="flex items-center">
+              <div class="text-white basis-2/12">
+                <iconify-icon icon="ant-design:pie-chart-outlined" class="text-6xl"></iconify-icon>
+              </div>
+              <div class="basis-10/12">
+                <h4 class="text-2xl font-bold text-white">Data Engineering</h4>
+                <p class="text-gray-400">Melakukan pengolahan data untuk dianalisa dan memperoleh manfaat</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  {{-- Division Section End --}}
+
+
+
+  {{-- Gallery Section Start --}}
+  <section>
+    <div class="container">
+      <div class="w-10/12 mx-auto">
+        <header class="flex items-center justify-center mb-20 text-center">
+          <div class="border border-gray-700 w-36"></div>
+          <h2 class="mx-5 text-5xl font-bold text-white">Galeri</h2>
+          <div class="border border-gray-700 w-36"></div>
+        </header>
+
+        <div class="w-full slick-slider">
+          <div class="mx-2 overflow-hidden rounded-xl h-72">
+            <img src="assets/images/img-2.png" alt="Image 1" class="object-cover w-full h-full">
+          </div>
+          <div class="mx-2 overflow-hidden h-72 rounded-xl">
+            <img src="assets/images/img-1.png" alt="Image 1" class="object-cover w-full h-full">
+          </div>
+          <div class="mx-2 overflow-hidden rounded-xl h-72">
+            <img src="assets/images/img-3.png" alt="Image 1" class="object-cover w-full h-full">
+          </div>
+          <div class="mx-2 overflow-hidden rounded-xl h-72">
+            <img src="assets/images/img-2.png" alt="Image 1" class="object-cover w-full h-full">
+          </div>
+          <div class="mx-2 overflow-hidden h-72 rounded-xl">
+            <img src="assets/images/img-1.png" alt="Image 1" class="object-cover w-full h-full">
+          </div>
+          <div class="mx-2 overflow-hidden rounded-xl h-72">
+            <img src="assets/images/img-3.png" alt="Image 1" class="object-cover w-full h-full">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  {{-- Gallery Section End --}}
+
+
+  {{-- Article Section Start --}}
+  <section class="my-52">
+    <div class="container">
+      <div class="w-10/12 mx-auto">
+        <header class="flex items-center justify-between mb-20">
+          <h2 class="mx-5 text-5xl font-bold text-white">Artikel</h2>
+          <a href="" class="text-gray-400">Lihat Semua <iconify-icon icon="ph:arrow-right"></iconify-icon>
+          </a>
+        </header>
+
+        <div class="grid grid-cols-3 gap-5">
+          <div class="rounded-xl bg-glass">
+            <div class="mb-2">
+              <img src="assets/images/article-1.png" alt="Thumbnail Article" class="w-full h-full">
+            </div>
+            <div class="p-5 text-white">
+              <h5 class="mb-2 text-xl font-bold">Bootstrap 5 VS Tailwind CSS</h5>
+              <p class="text-sm text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                asperiores magni fugit accusamus repudiandae amet cupiditate consectetur...</p>
+            </div>
+          </div>
+          <div class="rounded-xl bg-glass">
+            <div class="mb-2">
+              <img src="assets/images/article-1.png" alt="Thumbnail Article" class="w-full h-full">
+            </div>
+            <div class="p-5 text-white">
+              <h5 class="mb-2 text-xl font-bold">Bootstrap 5 VS Tailwind CSS</h5>
+              <p class="text-sm text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                asperiores magni fugit accusamus repudiandae amet cupiditate consectetur...</p>
+            </div>
+          </div>
+          <div class="rounded-xl bg-glass">
+            <div class="mb-2">
+              <img src="assets/images/article-1.png" alt="Thumbnail Article" class="w-full h-full">
+            </div>
+            <div class="p-5 text-white">
+              <h5 class="mb-2 text-xl font-bold">Bootstrap 5 VS Tailwind CSS</h5>
+              <p class="text-sm text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                asperiores magni fugit accusamus repudiandae amet cupiditate consectetur...</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  {{-- Article Section End --}}
+
 </div>
+
+@push('js-custom')
+  <script>
+    $(document).ready(function() {
+      $('.slick-slider').slick({
+        centerMode: true,
+        centerPadding: '300px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+    })
+  </script>
+@endpush
