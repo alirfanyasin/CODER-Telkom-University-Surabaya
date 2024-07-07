bff0b92d5f792a081c91583166955b84168cdfe6
alirfanyasin <risayunita21@gmail.com>
2024-07-07 13:52:01 +0700

    slicing: dashboard page

--- resources/views/livewire/app/dashboard.blade.php
+++ resources/views/livewire/app/dashboard.blade.php
@@ -1,3 +1,244 @@
 <div>
-    {{-- Do your work, then step back. --}}
+  <div class="flex">
+    <aside class="relative h-screen px-5 py-10 overflow-y-auto bg-glass/70 backdrop-blur-sm basis-3/12">
+      <div class="flex justify-center">
+        <img src="assets/images/logo/logo.png" alt="Logo" class="w-36">
+      </div>
+      <nav class="flex flex-col flex-wrap w-full p-6 hs-accordion-group" data-hs-accordion-always-open>
+        <ul class="mt-10">
+          <li class="mb-4">
+            <a href="" class="flex items-center px-8 py-3 font-medium bg-white rounded-lg text-md">
+              <iconify-icon icon="material-symbols:dashboard-outline" class="mr-3 text-2xl"></iconify-icon>
+              Dashboard
+            </a>
+          </li>
+          <li class="mb-4 hs-accordion" id="e-learning-accordion">
+            <button type="button"
+              class="hs-accordion-toggle hs-accordion-active:text-black hs-accordion-active:bg-white   hs-accordion-active:hover:bg-white w-full text-start flex items-center gap-x-3.5 py-3 hover:text-black hover:bg-white px-8 text-md font-medium text-gray-400 bg-transparent rounded-lg">
+              <iconify-icon icon="game-icons:spell-book" class="text-2xl"></iconify-icon>
+              E-Learning
+
+              <svg
+                class="hidden text-gray-600 hs-accordion-active:block ms-auto size-4 group-hover:text-gray-500 dark:text-neutral-400"
+                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m18 15-6-6-6 6" />
+              </svg>
+
+              <svg
+                class="block text-gray-600 hs-accordion-active:hidden ms-auto size-4 group-hover:text-gray-500 dark:text-neutral-400"
+                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m6 9 6 6 6-6" />
+              </svg>
+            </button>
+
+            <div id="e-learning-accordion"
+              class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden">
+              <ul class="pt-2 hs-accordion-group" data-hs-accordion-always-open>
+                <li class="hs-accordion" id="e-learning-accordion-sub-1">
+                  <a href=""
+                    class="flex py-3 font-medium text-gray-400 rounded-lg text-md item-center hover:bg-white hover:text-black ps-16">
+                    <iconify-icon icon="fluent-emoji-high-contrast:open-book" class="mr-3 text-2xl"></iconify-icon>
+                    Modul
+                  </a>
+                </li>
+                <li class="hs-accordion" id="e-learning-accordion-sub-2">
+                  <a href=""
+                    class="flex py-3 font-medium text-gray-400 rounded-lg text-md item-center hover:bg-white hover:text-black ps-16">
+                    <iconify-icon icon="bx:task" class="mr-3 text-2xl"></iconify-icon>
+                    Tugas
+                  </a>
+                </li>
+                <li class="hs-accordion" id="e-learning-accordion-sub-3">
+                  <a href=""
+                    class="flex py-3 font-medium text-gray-400 rounded-lg text-md item-center hover:bg-white hover:text-black ps-16">
+                    <iconify-icon icon="fluent:video-24-regular" class="mr-3 text-2xl"></iconify-icon>
+                    Pertemuan
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="mb-4">
+            <a href=""
+              class="flex items-center px-8 py-3 font-medium text-gray-400 bg-transparent rounded-lg hover:bg-white hover:text-black text-md">
+              <iconify-icon icon="ph:user-check" class="mr-3 text-2xl"></iconify-icon>
+              Presensi
+            </a>
+          </li>
+          <li class="mb-4">
+            <a href=""
+              class="flex items-center px-8 py-3 font-medium text-gray-400 bg-transparent rounded-lg hover:bg-white hover:text-black text-md">
+              <iconify-icon icon="ph:users-three" class="mr-3 text-2xl"></iconify-icon>
+              Member
+            </a>
+          </li>
+          <li class="mb-4">
+            <a href=""
+              class="flex items-center px-8 py-3 font-medium text-gray-400 bg-transparent rounded-lg hover:bg-white hover:text-black text-md">
+              <iconify-icon icon="teenyicons:attachment-outline" class="mr-3 text-2xl"></iconify-icon>
+              Laporan
+            </a>
+          </li>
+
+          <li class="mb-4 hs-accordion" id="event-accordion">
+            <button type="button"
+              class="hs-accordion-toggle hs-accordion-active:text-black hs-accordion-active:bg-white   hs-accordion-active:hover:bg-white w-full text-start flex items-center gap-x-3.5 py-3 hover:text-black hover:bg-white px-8 text-md font-medium text-gray-400 bg-transparent rounded-lg">
+              <iconify-icon icon="carbon:event" class="text-2xl"></iconify-icon>
+              Events
+
+              <svg
+                class="hidden text-gray-600 hs-accordion-active:block ms-auto size-4 group-hover:text-gray-500 dark:text-neutral-400"
+                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m18 15-6-6-6 6" />
+              </svg>
+
+              <svg
+                class="block text-gray-600 hs-accordion-active:hidden ms-auto size-4 group-hover:text-gray-500 dark:text-neutral-400"
+                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m6 9 6 6 6-6" />
+              </svg>
+            </button>
+
+            <div id="event-accordion"
+              class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden">
+              <ul class="pt-2 hs-accordion-group" data-hs-accordion-always-open>
+                <li class="hs-accordion" id="event-accordion-sub-1">
+                  <a href=""
+                    class="flex py-3 font-medium text-gray-400 rounded-lg text-md item-center hover:bg-white hover:text-black ps-16">
+                    <iconify-icon icon="ic:outline-announcement" class="mr-3 text-2xl"></iconify-icon>
+                    Pengumuman
+                  </a>
+                </li>
+                <li class="hs-accordion" id="event-accordion-sub-2">
+                  <a href=""
+                    class="flex py-3 font-medium text-gray-400 rounded-lg text-md item-center hover:bg-white hover:text-black ps-16">
+                    <iconify-icon icon="ph:user-plus" class="mr-3 text-2xl"></iconify-icon>
+                    Reqrutment
+                  </a>
+                </li>
+                <li class="hs-accordion" id="event-accordion-sub-3">
+                  <a href=""
+                    class="flex py-3 font-medium text-gray-400 rounded-lg text-md item-center hover:bg-white hover:text-black ps-16">
+                    <iconify-icon icon="mdi:todo-auto" class="mr-3 text-2xl"></iconify-icon>
+                    Management Event
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+
+    <main class="relative h-screen overflow-y-auto basis-9/12">
+      <div class="container px-8">
+        <div class="w-full mx-auto">
+          <nav class="flex justify-between my-8">
+            <div class="relative">
+              <input type="search" name="search" id="search"
+                class="py-3 font-light text-white rounded-lg ps-12 bg-glass backdrop-blur-sm w-96 placeholder:text-white"
+                placeholder="Search">
+              <iconify-icon icon="lucide:search" class="absolute text-xl text-white left-4 top-3 "></iconify-icon>
+            </div>
+
+            <div class="flex">
+              <div class="flex items-center justify-center w-12 h-12 text-2xl rounded-md bg-glass backdrop-blur-sm">
+                <iconify-icon icon="carbon:notification" class="text-white"></iconify-icon>
+              </div>
+              <div class="mx-4 border-l-2 border-white"></div>
+              <a href="" class="flex items-center">
+                <div class="w-12 h-12 rounded-full bg-glass me-3"></div>
+                <div class="">
+                  <h5 class="font-light text-white">Irfan Yasin</h5>
+                  <p class="text-xs font-light text-gray-400">Web Development</p>
+                </div>
+              </a>
+            </div>
+          </nav>
+
+
+          <section class="my-10">
+            <header>
+              <h2 class="text-3xl font-semibold text-white">Halo, Irfan Yasin</h2>
+              <p class="text-gray-400">Siap untuk belajar sekarang!</p>
+            </header>
+          </section>
+
+          <section class="mb-3">
+            <div class="grid grid-cols-3 gap-3">
+              <div class="p-6 rounded-xl bg-glass/70 backdrop-blur-sm">
+                <h3 class="font-semibold text-white text-8xl">10</h3>
+                <p class="text-gray-400">Total Pertemuan</p>
+              </div>
+              <div class="p-6 rounded-xl bg-glass/70 backdrop-blur-sm">
+                <h3 class="font-semibold text-white text-8xl">10</h3>
+                <p class="text-gray-400">Total Tugas</p>
+              </div>
+              <div class="p-6 rounded-xl bg-glass/70 backdrop-blur-sm">
+                <h3 class="font-semibold text-white text-8xl">10</h3>
+                <p class="text-gray-400">Total Projek</p>
+              </div>
+            </div>
+          </section>
+
+          <section class="mb-10">
+            <div class="grid grid-cols-3 gap-3">
+              <div class="col-span-2 p-6 rounded-xl bg-glass/70 backdrop-blur-sm">
+                <header class="mb-4">
+                  <h4 class="text-xl font-semibold text-white">Statistik Aktifitas</h4>
+                </header>
+              </div>
+              <div class="p-6 rounded-xl bg-glass/70 backdrop-blur-sm">
+                <header class="mb-4">
+                  <h4 class="text-xl font-semibold text-white">Jadwal Pertemuan</h4>
+                </header>
+                <div>
+                  <div class="p-4 mb-4 border border-gray-500 rounded-xl bg-glass/70 backdrop-blur-sm">
+                    <header class="flex items-center">
+                      <h5 class="text-sm text-white">12.00 - 14.00</h5>
+                      <div class="mx-2 border-l-2 border-white">|</div>
+                      <h5 class="text-sm text-white">29 September 2024</h5>
+                    </header>
+                    <p class="mt-5 font-light text-gray-400">Belajar menggunakan Framework Tailwind.</p>
+                    <a href=""
+                      class="flex items-center justify-center w-10 h-10 mt-4 border border-gray-500 rounded-lg">
+                      <iconify-icon icon="fluent:video-24-regular" class="text-3xl text-white"></iconify-icon>
+                    </a>
+                  </div>
+                  <div class="p-4 mb-4 border border-gray-500 rounded-xl bg-glass/70 backdrop-blur-sm">
+                    <header class="flex items-center">
+                      <h5 class="text-sm text-white">12.00 - 14.00</h5>
+                      <div class="mx-2 border-l-2 border-white">|</div>
+                      <h5 class="text-sm text-white">29 September 2024</h5>
+                    </header>
+                    <p class="mt-5 font-light text-gray-400">Belajar menggunakan Framework Tailwind.</p>
+                    <a href=""
+                      class="flex items-center justify-center w-10 h-10 mt-4 border border-gray-500 rounded-lg">
+                      <iconify-icon icon="fluent:video-24-regular" class="text-3xl text-white"></iconify-icon>
+                    </a>
+                  </div>
+                  <div class="p-4 mb-4 border border-gray-500 rounded-xl bg-glass/70 backdrop-blur-sm">
+                    <header class="flex items-center">
+                      <h5 class="text-sm text-white">12.00 - 14.00</h5>
+                      <div class="mx-2 border-l-2 border-white">|</div>
+                      <h5 class="text-sm text-white">29 September 2024</h5>
+                    </header>
+                    <p class="mt-5 font-light text-gray-400">Belajar menggunakan Framework Tailwind.</p>
+                    <a href=""
+                      class="flex items-center justify-center w-10 h-10 mt-4 border border-gray-500 rounded-lg">
+                      <iconify-icon icon="fluent:video-24-regular" class="text-3xl text-white"></iconify-icon>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </main>
+  </div>
 </div>
