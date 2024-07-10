0e5088bbcf44b5717bb79ed32b3204500521467a
alirfanyasin <risayunita21@gmail.com>
2024-07-10 12:59:40 +0700

    feat: view all meeting

--- resources/views/livewire/app/meeting.blade.php
+++ resources/views/livewire/app/meeting.blade.php
@@ -1,8 +1,73 @@
 <div>
+  {{-- Header Start --}}
+  <header class="flex items-center justify-between mb-5">
+    <h2 class="text-3xl font-bold text-white">Daftar Petemuan</h2>
+    <a href="" class="flex items-center px-5 py-3 text-sm font-semibold text-black bg-white rounded-md">Buat
+      Pertemuan <iconify-icon icon="mingcute:arrow-right-line" class="text-xl ms-2"></iconify-icon></a>
+  </header>
+  {{-- Header End --}}
+
 
   {{-- View All Meeting Section Start --}}
   <section>
-    <h1>Hello World</h1>
+    <a href="/app/e-learning/meetings/pertemuan-1" class="block">
+      <div class="p-5 mb-4 rounded-lg bg-glass">
+        <header class="flex items-center justify-between mb-3 text-white">
+          <div class="flex items-center">
+            <iconify-icon icon="iconoir:calendar" class="text-2xl me-3"></iconify-icon>
+            <span class="text-sm font-light">Minggu, 20 Mei 2024</span>
+            <span class="text-sm font-light ms-2"> - 08:00 WIB</span>
+          </div>
+          <div class="px-2 py-1 text-sm bg-green-600 rounded-md">
+            Selesai
+          </div>
+        </header>
+        <div>
+          <div>
+            <h3 class="text-2xl font-semibold text-white">Pertemuan 1</h3>
+            <p class="font-light text-gray-400">Mengenal Fundamental HTML dan CSS Dasar untuk Pemula</p>
+          </div>
+          <div class="flex items-center mt-4 ">
+            <a href=""
+              class="flex items-center justify-center w-10 h-10 border border-gray-500 rounded-lg group hover:bg-white">
+              <iconify-icon icon="fluent:video-24-regular"
+                class="text-3xl text-white group-hover:text-black"></iconify-icon>
+            </a>
+            <p class="text-white ms-3">Zoom</p>
+          </div>
+        </div>
+      </div>
+    </a>
+
+    <a href="" class="block">
+      <div class="p-5 mb-4 rounded-lg bg-glass">
+        <header class="flex items-center justify-between mb-3 text-white">
+          <div class="flex items-center">
+            <iconify-icon icon="iconoir:calendar" class="text-2xl me-3"></iconify-icon>
+            <span class="text-sm font-light">Minggu, 20 Mei 2024</span>
+            <span class="text-sm font-light ms-2"> - 08:00 WIB</span>
+          </div>
+          <div class="px-2 py-1 text-sm bg-green-600 rounded-md">
+            Selesai
+          </div>
+        </header>
+        <div>
+          <div>
+            <h3 class="text-2xl font-semibold text-white">Pertemuan 2</h3>
+            <p class="font-light text-gray-400">Mengenal Fundamental HTML dan CSS Dasar untuk Pemula</p>
+          </div>
+          <div class="flex items-center mt-4">
+            <div
+              class="flex items-center justify-center w-10 h-10 border border-gray-500 rounded-lg group hover:bg-white">
+              <iconify-icon icon="carbon:location" class="text-3xl text-white group-hover:text-black"></iconify-icon>
+            </div>
+            <p class="text-white ms-3">Lab. Komputer 1.12</p>
+          </div>
+        </div>
+      </div>
+    </a>
+
+
   </section>
   {{-- View All Meeting Section End --}}
 
