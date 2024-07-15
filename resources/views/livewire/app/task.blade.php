3da212f95dea8d5293e3b5e81f84af38dbb5a767
Arararra <55007364+Arararra@users.noreply.github.com>
2024-07-15 16:29:21 +0700

    add: meeting divider for tasks

--- resources/views/livewire/app/task.blade.php
+++ resources/views/livewire/app/task.blade.php
@@ -21,30 +21,40 @@
 
   {{-- View All Task Section Start --}}
   <section class="mb-10">
-    @for($i = 1; $i < 4; $i++)
-      <a href="" wire:navigate class="block">
-        <div class="p-5 mb-4 rounded-lg bg-glass">
-            <h3 class="text-2xl font-semibold text-white">Tugas {{ $i }}</h3>
-            <p class="font-light mt-4 text-gray-400">Membuat website menggunakan HTML dan CSS</p>
-
-          <div class="flex items-center justify-between w-full mt-4">
-            <div class="flex items-center w-full">
-              <a href="" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg bg-glass text-white hover:bg-white hover:text-black">
-                Lihat pengumpulan
-              </a>
-            </div>
-
-            <div class="flex items-center">
-              <a href="{{ route('app.e-learning.meeting.edit') }}" wire:navigate class="me-2">
-                <iconify-icon icon="uil:edit" class="text-xl text-gray-400"></iconify-icon>
-              </a>
-              <a href="">
-                <iconify-icon icon="tabler:trash" class="text-xl text-gray-400"></iconify-icon>
-              </a>
-            </div>
-          </div>
+    @for($i = 1; $i < 3; $i++)
+      <div>
+        <div class="flex items-center mb-4 text-white">
+          <iconify-icon icon="tabler:calendar-month" class="text-xl"></iconify-icon>
+          <h2 class="ms-2 me-3 text-sm font-light font-bold">Pertemuan {{ $i }}</h2>
+          <div class="flex-grow border border-gray-600"></div>
         </div>
-      </a>
+        
+        @for($j = 1; $j < 3; $j++)
+          <a href="" wire:navigate class="block">
+            <div class="p-5 mb-4 rounded-lg bg-glass">
+                <h3 class="text-2xl font-semibold text-white">Tugas {{ $j }}</h3>
+                <p class="font-light text-gray-400">Membuat website menggunakan HTML dan CSS</p>
+
+              <div class="flex items-center justify-between w-full mt-4">
+                <div class="flex items-center w-full">
+                  <a href="" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg bg-glass text-white hover:bg-white hover:text-black">
+                    Lihat pengumpulan
+                  </a>
+                </div>
+
+                <div class="flex items-center">
+                  <a href="{{ route('app.e-learning.meeting.edit') }}" wire:navigate class="me-2">
+                    <iconify-icon icon="uil:edit" class="text-xl text-gray-400"></iconify-icon>
+                  </a>
+                  <a href="">
+                    <iconify-icon icon="tabler:trash" class="text-xl text-gray-400"></iconify-icon>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </a>
+        @endfor
+      </div>
     @endfor
   </section>
   {{-- View All Task Section End --}}
