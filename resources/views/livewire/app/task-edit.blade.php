6fc3983d6b2153e1cf7b6bce15440d0adfef6a53
alirfanyasin <risayunita21@gmail.com>
2024-07-20 20:55:53 +0700

    fix: modul and task form

--- resources/views/livewire/app/task-edit.blade.php
+++ resources/views/livewire/app/task-edit.blade.php
@@ -1,29 +1,38 @@
 <div>
-  <header class="flex items-center justify-between my-7">
-    <h2 class="text-2xl font-bold text-white md:text-3xl">Perbarui Tugas</h2>
+  {{-- Header Start --}}
+  <header class="flex my-7">
+    <h2 class="text-2xl font-bold text-white md:text-3xl">Edit Tugas</h2>
   </header>
+  {{-- Header End --}}
 
+  {{-- Informasi Modul Start --}}
   <form action="" class="mb-10">
-    <section action="" class="p-5 mb-4 rounded-lg text-white bg-glass">
-      <h3 class="text-2xl font-semibold mb-4">Informasi Tugas</h3>
-      
-      <div class="grid lg:grid-cols-3 gap-4 mb-4">
+    <section action="" class="p-6 mb-6 text-white rounded-lg bg-glass">
+      <header class="flex items-center justify-between mb-8 text-white">
+        <div class="flex items-center">
+          <a href="{{ route('app.e-learning.task') }}" wire:navigate
+            class="flex items-center justify-center w-8 h-8 rounded-full hover:border me-2 bg-lightGray">
+            <iconify-icon icon="carbon:arrow-left"></iconify-icon>
+          </a>
+          <h4 class="text-xl font-semibold">Informasi Tugas</h4>
+        </div>
+      </header>
+
+      <div class="grid gap-4 mb-4 lg:grid-cols-3">
         <div>
-          <label for="task_name" class="block text-sm font-medium leading-6 mb-2">Nama Tugas</label>
-          <input type="text" name="task_name" id="task_name" 
-            class="py-2 px-4 block w-full border-gray-200 bg-[#27292C] rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" 
-            placeholder="Nama Tugas" value="Fundamental HTML dan CSS Dasar">
+          <label for="task_name" class="block mb-2 text-sm font-medium leading-6">Nama Tugas</label>
+          <input type="text" name="task_name" id="task_name"
+            class="block w-full p-3 text-white rounded-lg bg-lightGray" placeholder="Nama Tugas"
+            value="Fundamental HTML dan CSS Dasar">
         </div>
         <div>
-          <label for="task_due_date" class="block text-sm font-medium leading-6 mb-2">Batas Pengumpulan</label>
-          <input type="datetime-local" name="task_due_date" id="task_due_date" autocomplete="task_due_date" 
-            class="py-2 px-4 block w-full border-gray-200 bg-[#27292C] rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" 
-            value="2024-07-15T19:30">
+          <label for="task_due_date" class="block mb-2 text-sm font-medium leading-6">Batas Pengumpulan</label>
+          <input type="datetime-local" name="task_due_date" id="task_due_date" autocomplete="task_due_date"
+            class="block w-full p-3 text-white rounded-lg bg-lightGray" value="2024-07-15T19:30">
         </div>
         <div>
-          <label for="task_meeting" class="block text-sm font-medium leading-6 mb-2">Pertemuan</label>
-          <select name="task_meeting" id="task_meeting"
-            class="py-2 px-4 block w-full border-gray-200 bg-[#27292C] rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" 
+          <label for="task_meeting" class="block mb-2 text-sm font-medium leading-6">Pertemuan</label>
+          <select name="task_meeting" id="task_meeting" class="block w-full p-3 text-white rounded-lg bg-lightGray"
             placeholder="Pertemuan" value="pertemuan-1">
             <option value="pertemuan-1">Pertemuan 1</option>
             <option value="pertemuan-2">Pertemuan 2</option>
@@ -31,35 +40,42 @@
         </div>
       </div>
 
-      <label for="task_description" class="block text-sm font-medium leading-6 mb-2">Deskripsi Tugas</label>
-      <textarea name="task_description" id="task_description" class="py-2 px-4 block w-full border-gray-200 bg-[#27292C] rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">Mengenal fundamental HTML dan CSS untuk pemula</textarea>
-    </section>
 
-    <section action="" class="p-5 mb-4 rounded-lg text-white bg-glass">
-      <h3 class="text-2xl font-semibold mb-4">Informasi Tugas</h3>
-      
-      <div class="grid lg:grid-cols-3 gap-4 mb-4">
+      <label for="task_description" class="block mb-2 text-sm font-medium leading-6">Deskripsi Tugas</label>
+      <textarea name="task_description" id="task_description" class="block w-full p-3 text-white rounded-lg bg-lightGray"
+        rows="5">Mengenal fundamental HTML dan CSS untuk pemula</textarea>
+    </section>
+    {{-- Informasi Modul End --}}
+
+    <section action="" class="p-5 mb-4 text-white rounded-lg bg-glass">
+      <div class="grid gap-4 mb-4 lg:grid-cols-2">
         <div class="lg:col-span-1">
-          <label for="task_file_type" class="block text-sm font-medium leading-6 mb-2">Jenis File Pendukung</label>
-          <select name="task_file_type" id="task_file_type"
-            class="py-2 px-4 block w-full border-gray-200 bg-[#27292C] rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" 
+          <label for="task_file_type" class="block mb-2 text-sm font-medium leading-6">Jenis File Pendukung</label>
+          <select name="task_file_type" id="task_file_type" class="block w-full p-3 text-white rounded-lg bg-lightGray"
             placeholder="File type" value="docs">
             <option value="docs">Dokumen (PDF, DOCS, dan Lainnya)</option>
             <option value="img">Image (JPG, PNG, dan Lainnya)</option>
           </select>
         </div>
-        <div class="lg:col-span-2">
-          <label for="task_file" class="block text-sm font-medium leading-6 mb-2">Upload File <span class="text-xs text-gray-400">(Maksimal 5 Mb)</span></label>
-          
-          <input type="file" name="task_file" id="task_file" 
-            class="py-2 px-4 block w-full border-gray-200 bg-[#27292C] rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" >
+        <div class="">
+          <div class="w-full">
+            <label for="input-label" class="block mb-2 font-light dark:text-white">Upload File <span
+                class="text-[#9E9E9E] text-xs">(Maksimal 5 Mb)</span></label>
+            <input type="file" id="file-input" class="hidden">
+            <label for="file-input"
+              class="flex items-center gap-2 p-3 text-white rounded-md cursor-pointer bg-lightGray">
+              <span class="text-xs bg-[#43474C] py-1 px-1.5">Pilih File</span>
+              <span class="text-xs" id="file-name">Belum ada file yang dipilih.</span>
+            </label>
+          </div>
+
         </div>
       </div>
     </section>
 
     <section class="flex items-center justify-end">
       <a href="{{ route('app.e-learning.task') }}" wire:navigate
-        class="flex items-center px-5 py-3 me-4 text-sm font-semibold text-gray-300 bg-glass hover:text-black hover:bg-white rounded-md">
+        class="flex items-center px-5 py-3 text-sm font-semibold text-gray-300 rounded-md me-4 bg-glass hover:text-black hover:bg-white">
         Batal
       </a>
       <button class="flex items-center px-5 py-3 text-sm font-semibold text-black bg-white rounded-md">
@@ -73,8 +89,8 @@
   <script>
     const styleElement = document.createElement('style');
     styleElement.textContent = `::-webkit-calendar-picker-indicator {
-      filter: invert(1);
-    }`;
+        filter: invert(1);
+      }`;
     document.head.appendChild(styleElement);
   </script>
 @endpush
