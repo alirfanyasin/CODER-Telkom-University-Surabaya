71c4012a27b611af9db48cab1c27f13b50aa9a43
alirfanyasin <risayunita21@gmail.com>
2024-08-07 09:10:39 +0700

    feat: authorization between roles

--- resources/views/livewire/app/meeting.blade.php
+++ resources/views/livewire/app/meeting.blade.php
@@ -3,17 +3,19 @@
   <header class="flex items-center justify-between my-7">
     <h2 class="text-2xl font-bold text-white md:text-3xl">Daftar Pertemuan</h2>
 
-    <div class="hidden md:block">
-      <a href="{{ route('app.e-learning.meeting.create') }}" wire:navigate
-        class="flex items-center px-5 py-3 text-sm font-semibold text-black bg-white rounded-md">Buat
-        Pertemuan <iconify-icon icon="mingcute:arrow-right-line" class="text-xl ms-2"></iconify-icon></a>
-    </div>
+    @role(['admin'])
+      <div class="hidden md:block">
+        <a href="{{ route('app.e-learning.meeting.create') }}" wire:navigate
+          class="flex items-center px-5 py-3 text-sm font-semibold text-black bg-white rounded-md">Buat
+          Pertemuan <iconify-icon icon="mingcute:arrow-right-line" class="text-xl ms-2"></iconify-icon></a>
+      </div>
 
-    <div class="block md:hidden">
-      <a href="{{ route('app.e-learning.meeting.create') }}" wire:navigate
-        class="flex items-center justify-center w-10 h-10 text-sm font-semibold text-black bg-white rounded-full"><iconify-icon
-          icon="majesticons:plus-line" class="text-2xl"></iconify-icon></a>
-    </div>
+      <div class="block md:hidden">
+        <a href="{{ route('app.e-learning.meeting.create') }}" wire:navigate
+          class="flex items-center justify-center w-10 h-10 text-sm font-semibold text-black bg-white rounded-full"><iconify-icon
+            icon="majesticons:plus-line" class="text-2xl"></iconify-icon></a>
+      </div>
+    @endrole
   </header>
   {{-- Header End --}}
 
@@ -48,20 +50,22 @@
             </div>
             {{-- Icon meeting type end --}}
 
-            {{-- Icon action start --}}
-            <div class="inline md:flex md:justify-between">
-              <div class="flex gap-2 text-gray-400 md:gap-4">
-                <a href=""
-                  class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-red-600 border-[#27272A] px-2 md:px-4 py-1">
-                  <iconify-icon icon="tabler:trash"></iconify-icon><span class="hidden md:block">Hapus</span>
-                </a>
-                <a href=""
-                  class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-yellow-600 border-[#27272A] px-2 md:px-4 py-1">
-                  <iconify-icon icon="lucide:edit"></iconify-icon><span class="hidden md:block">Edit</span>
-                </a>
+            @role(['admin'])
+              {{-- Icon action start --}}
+              <div class="inline md:flex md:justify-between">
+                <div class="flex gap-2 text-gray-400 md:gap-4">
+                  <a href=""
+                    class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-red-600 border-[#27272A] px-2 md:px-4 py-1">
+                    <iconify-icon icon="tabler:trash"></iconify-icon><span class="hidden md:block">Hapus</span>
+                  </a>
+                  <a href=""
+                    class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-yellow-600 border-[#27272A] px-2 md:px-4 py-1">
+                    <iconify-icon icon="lucide:edit"></iconify-icon><span class="hidden md:block">Edit</span>
+                  </a>
+                </div>
               </div>
-            </div>
-            {{-- Icon action end --}}
+              {{-- Icon action end --}}
+            @endrole
           </div>
         </div>
       </div>
@@ -94,20 +98,22 @@
             </div>
             {{-- Icon meeting type end --}}
 
-            {{-- Icon action start --}}
-            <div class="inline md:flex md:justify-between">
-              <div class="flex gap-2 text-gray-400 md:gap-4">
-                <a href=""
-                  class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-red-600 border-[#27272A] px-2 md:px-4 py-1">
-                  <iconify-icon icon="tabler:trash"></iconify-icon><span class="hidden md:block">Hapus</span>
-                </a>
-                <a href=""
-                  class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-yellow-600 border-[#27272A] px-2 md:px-4 py-1">
-                  <iconify-icon icon="lucide:edit"></iconify-icon><span class="hidden md:block">Edit</span>
-                </a>
+            @role(['admin'])
+              {{-- Icon action start --}}
+              <div class="inline md:flex md:justify-between">
+                <div class="flex gap-2 text-gray-400 md:gap-4">
+                  <a href=""
+                    class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-red-600 border-[#27272A] px-2 md:px-4 py-1">
+                    <iconify-icon icon="tabler:trash"></iconify-icon><span class="hidden md:block">Hapus</span>
+                  </a>
+                  <a href=""
+                    class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-yellow-600 border-[#27272A] px-2 md:px-4 py-1">
+                    <iconify-icon icon="lucide:edit"></iconify-icon><span class="hidden md:block">Edit</span>
+                  </a>
+                </div>
               </div>
-            </div>
-            {{-- Icon action end --}}
+              {{-- Icon action end --}}
+            @endrole
           </div>
         </div>
       </div>
@@ -140,20 +146,22 @@
             </div>
             {{-- Icon meeting type end --}}
 
-            {{-- Icon action start --}}
-            <div class="inline md:flex md:justify-between">
-              <div class="flex gap-2 text-gray-400 md:gap-4">
-                <a href=""
-                  class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-red-600 border-[#27272A] px-2 md:px-4 py-1">
-                  <iconify-icon icon="tabler:trash"></iconify-icon><span class="hidden md:block">Hapus</span>
-                </a>
-                <a href=""
-                  class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-yellow-600 border-[#27272A] px-2 md:px-4 py-1">
-                  <iconify-icon icon="lucide:edit"></iconify-icon><span class="hidden md:block">Edit</span>
-                </a>
+            @role(['admin'])
+              {{-- Icon action start --}}
+              <div class="inline md:flex md:justify-between">
+                <div class="flex gap-2 text-gray-400 md:gap-4">
+                  <a href=""
+                    class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-red-600 border-[#27272A] px-2 md:px-4 py-1">
+                    <iconify-icon icon="tabler:trash"></iconify-icon><span class="hidden md:block">Hapus</span>
+                  </a>
+                  <a href=""
+                    class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-yellow-600 border-[#27272A] px-2 md:px-4 py-1">
+                    <iconify-icon icon="lucide:edit"></iconify-icon><span class="hidden md:block">Edit</span>
+                  </a>
+                </div>
               </div>
-            </div>
-            {{-- Icon action end --}}
+              {{-- Icon action end --}}
+            @endrole
           </div>
         </div>
       </div>
--- resources/views/livewire/app/task.blade.php
+++ resources/views/livewire/app/task.blade.php
@@ -2,20 +2,21 @@
   {{-- Header Start --}}
   <header class="flex items-center justify-between my-7">
     <h2 class="text-2xl font-bold text-white md:text-3xl">Daftar Tugas</h2>
+    @role(['admin'])
+      <div class="hidden md:block">
+        <a href="{{ route('app.e-learning.task.create') }}" wire:navigate
+          class="flex items-center px-5 py-3 text-sm font-semibold text-black bg-white rounded-md">
+          Buat Tugas Baru <iconify-icon icon="mingcute:arrow-right-line" class="text-xl ms-2"></iconify-icon>
+        </a>
+      </div>
 
-    <div class="hidden md:block">
-      <a href="{{ route('app.e-learning.task.create') }}" wire:navigate
-        class="flex items-center px-5 py-3 text-sm font-semibold text-black bg-white rounded-md">
-        Buat Tugas Baru <iconify-icon icon="mingcute:arrow-right-line" class="text-xl ms-2"></iconify-icon>
-      </a>
-    </div>
-
-    <div class="block md:hidden">
-      <a href="{{ route('app.e-learning.task.create') }}" wire:navigate
-        class="flex items-center justify-center w-10 h-10 text-sm font-semibold text-black bg-white rounded-full">
-        <iconify-icon icon="majesticons:plus-line" class="text-2xl"></iconify-icon>
-      </a>
-    </div>
+      <div class="block md:hidden">
+        <a href="{{ route('app.e-learning.task.create') }}" wire:navigate
+          class="flex items-center justify-center w-10 h-10 text-sm font-semibold text-black bg-white rounded-full">
+          <iconify-icon icon="majesticons:plus-line" class="text-2xl"></iconify-icon>
+        </a>
+      </div>
+    @endrole
   </header>
   {{-- Header End --}}
 
@@ -35,26 +36,43 @@
               <h3 class="text-2xl font-semibold text-white">Tugas {{ $j }}</h3>
               <p class="font-light text-gray-400">Membuat website menggunakan HTML dan CSS</p>
 
-              <div class="flex items-center justify-between w-full mt-4">
-                <div class="flex items-center w-full">
-                  <a href="{{ route('app.e-learning.task.submission') }}" wire:navigate
-                    class="inline-flex items-center px-4 py-3 text-sm font-semibold text-white rounded-lg gap-x-2 bg-glass hover:bg-white hover:text-black">
-                    Lihat pengumpulan
-                  </a>
-                </div>
-
-                <div class="inline md:flex md:justify-between">
-                  <div class="flex gap-2 text-gray-400 md:gap-4">
-                    <a href=""
-                      class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-red-600 border-[#27272A] px-2 md:px-4 py-1">
-                      <iconify-icon icon="tabler:trash"></iconify-icon><span class="hidden md:block">Hapus</span>
-                    </a>
-                    <a href=""
-                      class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-yellow-600 border-[#27272A] px-2 md:px-4 py-1">
-                      <iconify-icon icon="lucide:edit"></iconify-icon><span class="hidden md:block">Edit</span>
+              <div class="mt-4">
+                @role(['admin'])
+                  <div class="flex items-center w-full">
+                    <a href="{{ route('app.e-learning.task.submission') }}" wire:navigate
+                      class="inline-flex items-center px-4 py-3 text-sm font-semibold text-white rounded-lg gap-x-2 bg-glass hover:bg-white hover:text-black">
+                      Lihat pengumpulan
                     </a>
                   </div>
-                </div>
+                @endrole
+                @role(['user'])
+                  <div class="flex items-center justify-between">
+                    <div class="flex items-center w-full">
+                      <a href="{{ route('app.e-learning.task.submission') }}" wire:navigate
+                        class="inline-flex items-center px-4 py-3 text-sm font-semibold text-white rounded-lg gap-x-2 bg-glass hover:bg-white hover:text-black">
+                        Kumpulkan Tugas
+                      </a>
+                    </div>
+                    <div>
+                      <p class="text-slate-400">10:00:30</p>
+                    </div>
+                  </div>
+                @endrole
+
+                @role(['admin'])
+                  <div class="inline md:flex md:justify-between">
+                    <div class="flex gap-2 text-gray-400 md:gap-4">
+                      <a href=""
+                        class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-red-600 border-[#27272A] px-2 md:px-4 py-1">
+                        <iconify-icon icon="tabler:trash"></iconify-icon><span class="hidden md:block">Hapus</span>
+                      </a>
+                      <a href=""
+                        class="flex gap-1 rounded-md items-center text-base font-medium border hover:text-yellow-600 border-[#27272A] px-2 md:px-4 py-1">
+                        <iconify-icon icon="lucide:edit"></iconify-icon><span class="hidden md:block">Edit</span>
+                      </a>
+                    </div>
+                  </div>
+                @endrole
               </div>
             </div>
           </a>
