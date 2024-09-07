3ac90fd2814c59dffac33ff0badb01bcfe634e92
alirfanyasin <risayunita21@gmail.com>
2024-09-07 19:13:53 +0700

    feat: edit and update points

--- app/Livewire/App/Points/Points.php
+++ app/Livewire/App/Points/Points.php
@@ -12,6 +12,33 @@ use Livewire\Component;
 
 class Points extends Component
 {
+    public $points;
+    public $times;
+    public $selectedPointId;
+
+    public function mount()
+    {
+        $this->points = null;
+        $this->times = null;
+        $this->selectedPointId = null;
+    }
+
+    public function openModal($id)
+    {
+        $data = ModelsPoints::find($id);
+        $this->points = $data->points;
+        $this->times = $data->times;
+
+        $this->selectedPointId = $id;
+    }
+
+    public function update()
+    {
+        if ($this->selectedPointId) {
+            ModelsPoints::find($this->selectedPointId)->update(['points' => $this->points, 'times' => $this->times]);
+        }
+    }
+
     public function render()
     {
         $data = ModelsPoints::all();
