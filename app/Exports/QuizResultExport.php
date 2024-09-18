2c62f25b3b04eb746d6270f14da9108389b2d8dc
alirfanyasin <risayunita21@gmail.com>
2024-09-18 10:08:41 +0700

    feat: export quiz result by division

---
+++ app/Exports/QuizResultExport.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Quiz\UserAnswerQuiz as QuizUserAnswerQuiz;
+use Illuminate\Support\Facades\Auth;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class QuizResultExport implements FromCollection, WithHeadings
+{
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function collection()
+    {
+        $quizResult = QuizUserAnswerQuiz::join('users', 'users.id', '=', 'user_answer_quizzes.user_id')
+            ->join('quizzes', 'quizzes.id', '=', 'user_answer_quizzes.quiz_id')
+            ->where('users.division_id', Auth::user()->division_id)
+            ->select('users.name', 'quizzes.title', 'user_answer_quizzes.grade', 'user_answer_quizzes.score')
+            ->get();
+
+        return $quizResult;
+    }
+
+    /**
+     * Define the header row
+     *
+     * @return array
+     */
+
+    public function headings(): array
+    {
+        return [
+            'NAMA',
+            'JUDUL KUIS',
+            'NILAI',
+            'GRADE',
+        ];
+    }
+}
--- resources/views/livewire/app/quiz/quiz.blade.php
+++ resources/views/livewire/app/quiz/quiz.blade.php
@@ -2,20 +2,44 @@
   {{-- Header Start --}}
   <header class="flex items-center justify-between my-7">
     <h2 class="text-2xl font-bold text-white md:text-3xl">Daftar Kuis</h2>
-
     @role(['admin'])
-      <div class="hidden md:block">
-        <a href="{{ route('app.e-learning.quiz.create') }}" wire:navigate
-          class="flex items-center px-5 py-3 text-sm font-semibold text-black bg-white rounded-md">Buat Kuis
-          <iconify-icon icon="mingcute:arrow-right-line" class="text-xl ms-2"></iconify-icon></a>
-      </div>
+      <div class="flex">
+        <div class="hidden md:block me-3">
+          <button wire:click.prevent='exportQuizResult' wire:loading.remove
+            class="flex items-center px-5 py-3 text-sm font-semibold text-white border rounded-md">
+            <iconify-icon icon="lets-icons:export" class="text-xl me-2"></iconify-icon>
+            Export</button>
 
-      <div class="block md:hidden">
-        <a href="{{ route('app.e-learning.quiz.create') }}" wire:navigate
-          class="flex items-center justify-center w-10 h-10 text-sm font-semibold text-black bg-white rounded-full"><iconify-icon
-            icon="majesticons:plus-line" class="text-2xl"></iconify-icon></a>
+          <div wire:loading wire:target="exportQuizResult"
+            class="flex items-center justify-center px-12 py-3 text-sm font-semibold border rounded-md">
+            <div
+              class="animate-spin inline-block size-4 border-[3px] border-current border-t-transparent text-white rounded-full"
+              role="status" aria-label="loading">
+              <span class="sr-only">Loading...</span>
+            </div>
+          </div>
+        </div>
+        <div class="block md:hidden">
+          <button wire:click.prevent='exportQuizResult'
+            class="flex items-center justify-center w-10 h-10 text-sm font-semibold text-black bg-white rounded-md">
+            <iconify-icon icon="lets-icons:export" class="text-2xl"></iconify-icon></button>
+        </div>
+
+
+        <div class="hidden md:block">
+          <a href="{{ route('app.e-learning.quiz.create') }}" wire:navigate
+            class="flex items-center px-5 py-3 text-sm font-semibold text-black bg-white rounded-md">Buat Kuis
+            <iconify-icon icon="mingcute:arrow-right-line" class="text-xl ms-2"></iconify-icon></a>
+        </div>
+
+        <div class="block md:hidden">
+          <a href="{{ route('app.e-learning.quiz.create') }}" wire:navigate
+            class="flex items-center justify-center w-10 h-10 text-sm font-semibold text-black bg-white rounded-full"><iconify-icon
+              icon="majesticons:plus-line" class="text-2xl"></iconify-icon></a>
+        </div>
       </div>
     @endrole
+
   </header>
   {{-- Header End --}}
 
