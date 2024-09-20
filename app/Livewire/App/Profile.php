c737b26c3ad8dac812485173339872ce1227854b
alirfanyasin <risayunita21@gmail.com>
2024-09-21 01:14:39 +0700

    feat: generate pdf

--- app/Livewire/App/Profile.php
+++ app/Livewire/App/Profile.php
@@ -2,9 +2,13 @@
 
 namespace App\Livewire\App;
 
+use App\Models\ActivityLetter;
 use App\Models\User;
 use App\Models\UserPoints;
+use Barryvdh\DomPDF\Facade\Pdf;
+// use PDF;
 use Illuminate\Support\Facades\Auth;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -14,6 +18,38 @@ use Livewire\Component;
 
 class Profile extends Component
 {
+    use LivewireAlert;
+
+    public function downloadActivityLetter()
+    {
+        $letter = ActivityLetter::find(1);
+
+        $data = [
+            'reference_number' => $letter->reference_number,
+            'date_published' => $letter->date_published,
+            'length_of_service' => $letter->length_of_service,
+            'leaders_name' => $letter->leaders_name,
+            'nim' => $letter->nim
+        ];
+
+        // dd($data);
+
+        $pdf = Pdf::loadView('pdf.activityLetter', $data);
+
+        return response()->streamDownload(function () use ($pdf) {
+            echo $pdf->stream();
+        }, Auth::user()->name . ' - ' . 'Surat Keaktifan CODER.pdf');
+
+        return $pdf->download('coder.pdf');
+
+        $this->alert('success', 'Berhasil download sertifikat', [
+            'position' => 'top-end',
+            'timer' => 3000,
+            'toast' => true,
+            'text' => '',
+            'timerProgressBar' => true,
+        ]);
+    }
 
     public function render()
     {
---
+++ resources/views/pdf/activityLetter.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Hello</title>
+</head>
+
+<body>
+  <h1>{{ $reference_number }}</h1>
+  <h1>{{ $date_published }}</h1>
+  <h1>{{ $length_of_service }}</h1>
+  <h1>{{ $leaders_name }}</h1>
+  <h1>{{ $nim }}</h1>
+</body>
+
+</html>
