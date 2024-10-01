40881b0c4dc29ae71003d02e3cc7138a0d6096ef
alirfanyasin <risayunita21@gmail.com>
2024-10-01 11:47:08 +0700

    clear task

--- app/Livewire/App/TaskDetail.php
+++ app/Livewire/App/TaskDetail.php
@@ -12,29 +12,9 @@ use Livewire\Component;
 
 class TaskDetail extends Component
 {
-    public $task;
-    public $task_name;
-    public $task_due_date;
-    public $task_meeting;
-    public $task_description;
 
     public function render()
     {
         return view('livewire.app.task-detail', ['task' => $this->task]);
-    }
-
-    public function mount()
-    {
-        $this->task = TaskModel::where('slug', request()->slug)->first();
-
-        $this->task_name = $this->task->name;
-        $this->task_due_date = $this->task->due_date;
-        $this->task_meeting = $this->operateTaskSection($this->task->section);
-        $this->task_description = $this->task->description;
-    }
-
-    protected function operateTaskSection($section)
-    {
-        return strtolower(str_replace(' ', '-', $section));
     }
 }
